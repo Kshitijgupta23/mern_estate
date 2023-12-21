ca4022e5c7efecb53b84e4a71eb1c95404d9ca67
Kshitij Gupta <kshitij.gupta2301@gmail.com>
2023-12-22 03:01:30 +0530

    Connected to database

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,12 +1,7 @@
-import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-const app = express();
-dotenv.config();
 
 const DB = process.env.URI;
-const PORT = process.env.PORT;
+console.log('DB:', process.env.URI);
 
 const connect = mongoose.connect(DB, {
   useNewUrlParser: true,
@@ -15,6 +10,4 @@ const connect = mongoose.connect(DB, {
   console.log("Connection successful");
 }).catch((err) => console.error("Connection failed", err));
 
-app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
-});
+export default connect;
