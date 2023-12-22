8e58ec2cba1af6352d47f6f39fcb60e47bd6f293
Kshitij Gupta <kshitij.gupta2301@gmail.com>
2023-12-23 01:39:05 +0530

    Fixed signout and signup page error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import {Home, About, Profile, SignIn, SignOut} from "./Pages";
+import {Home, About, Profile, SignIn, SignUp} from "./Pages";
 import Header from "./Components/Header";
 
 const App = () => {
@@ -11,7 +11,8 @@ const App = () => {
         <Route exact path="/about" element={<About />}/>
         <Route exact path="/profile" element={<Profile />}/>
         <Route exact path="/sign-in" element={<SignIn />}/>
-        <Route exact path="/sign-out" element={<SignOut />}/>
+        <Route exact path="/sign-up" element={<SignUp />}/>
+
       </Routes>
     </BrowserRouter>
     
