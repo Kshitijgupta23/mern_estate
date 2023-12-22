d584d9987f74a5134a634d76e4896095ac65414c
Kshitij Gupta <kshitij.gupta2301@gmail.com>
2023-12-23 01:03:23 +0530

    Created singup API route

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.routes.js';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 const app = express();
@@ -16,8 +17,11 @@ const connect = mongoose.connect(DB, {
   console.log("Connection successful");
 }).catch((err) => console.error("Connection failed", err));
 
-app.use('/api/user', userRouter);
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
