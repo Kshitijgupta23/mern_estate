f8b05d590052bac95cbdf8dbebe523cf4afebf07
Kshitij Gupta <kshitij.gupta2301@gmail.com>
2024-01-11 00:57:12 +0530

    Added pre installation for hosting

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 const app = express();
@@ -19,6 +20,8 @@ const connect = mongoose.connect(DB, {
   console.log("Connection successful");
 }).catch((err) => console.error("Connection failed", err));
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -29,6 +32,12 @@ app.listen(PORT, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname, 'client','dist', 'index.html'));
+});
 
 app.use((err, req, res, next) =>{
   const statusCode = err.statusCode || 500;
